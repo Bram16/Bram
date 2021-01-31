0671f45d0797263590c74869d74000646ddb5e61
“Joe <josephbramwell@bolstersystems.com>
2021-01-31 15:25:12 +0000

    portfolio setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../components/App.js";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Bram
+        <br />
+        Back soon
+      </p>
+    </div>
+  );
+}
+
+export default App;
